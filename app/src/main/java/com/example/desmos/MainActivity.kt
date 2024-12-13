2ec8a4695f9c9ab3fa6543928f9a61b5d4b6d5b7
rodomanovt <timondeseptikon@gmail.com>
2024-12-13 13:14:34 +0300

    v1.0

--- app/src/main/java/com/example/desmos/MainActivity.kt
+++ app/src/main/java/com/example/desmos/MainActivity.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.view.View
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.example.desmos.databinding.ActivityMainBinding
